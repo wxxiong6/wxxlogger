11b574fb9d815e1cb3052c862d7a0217d77f7579
vip6111111 <601115211@qq.com>
2017-11-10 10:35:58 +0800

    调整目录

--- Logger.php
+++ Logger.php
@@ -1,67 +1,57 @@
 <?php
-namespace Logger;
+namespace xwxlogger;
 use Exception;
-
 /**
  * 日志类
- * 需要手动创建日志目录,默认日志文件名是application.log。
- *  设置配置文件
- *  Log::getInstance()->setConfig();
- *
+ * 需要手动创建日志目录,
+ * 默认当前目录
+ * 默认日志文件名是application.log。
+ * @author wxxiong <wxxiong6@gmail.com>
  * @version   v1.2
  */
 class Logger
 {
-
     /**
      *  代表发生了最严重的错误，会导致整个服务停止（或者需要整个服务停止）。
      *  简单地说就是服务死掉了。
      * @var string
      */
     const LEVEL_FATAL = 'fatal';
-
     /**
      *  代表发生了必须马上处理的错误。此类错误出现以后可以允许程序继续运行，
      *  但必须马上修正，如果不修正，就会导致不能完成相应的业务。
      * @var string
      */
     const LEVEL_ERROR   = 'error';
-
     /**
      * 发生这个级别问题时，处理过程可以继续，但必须对这个问题给予额外关注。
      * @var string
      */
     const LEVEL_WARN = 'warn';
-
     /**
      *  此输出级别常用语业务事件信息。例如某项业务处理完毕，
      *  或者业务处理过程中的一些信息。
      * @var string
      */
     const LEVEL_INFO    = 'info';
-
     /**
      * 此输出级别用于开发阶段的调试，可以是某几个逻辑关键点的变量值的输出，
      * 或者是函数返回值的验证等等。业务相关的请用info
      * @var string
      */
     const LEVEL_DEBUG   = 'debug';
-
     /**
      * @var integer 日志内存最大行数
      */
     public $autoFlush = 10000;
-
     /**
      * @var array 日志信息
      */
     private $_logs = [];
-
     /**
      * @var integer 数量的日志消息
      */
     private $_logCount = 0;
-
     /**
      * @var int 限制返回堆栈帧的数量
      */
@@ -70,36 +60,29 @@ class Logger
      * @var integer maximum log file size
      */
     private $_maxFileSize = 1024; // in KB
-
     /**
      * @var integer 最大日志文件数
      */
     private $_maxLogFiles = 5;
-
     /**
      * @var string 日志文件目录
      */
     private $_logPath;
-
     /**
      * @var string 日志文件名称
      */
     private $_logFile = 'application.log';
-
     /**
      * @var object
      */
     private static $_instance;
-
     private function __construct(){}
     private function __clone(){}
-
     public function __set($name, $value)
     {
         $property = '_'.$name;
         $this->$property = $value;
     }
-
     public function __get($name)
     {
         return $this->$name;
@@ -114,7 +97,6 @@ class Logger
         }
         return self::$_instance;
     }
-
     /**
      * 设置配置文件
      * @param array $config
@@ -127,7 +109,6 @@ class Logger
             }
         }
     }
-
     /**
      * @return string directory storing log files. Defaults to application runtime path.
      */
@@ -137,7 +118,6 @@ class Logger
             $this->setLogPath(__DIR__);
         return $this->_logPath;
     }
-
     /**
      *  设置日志目录
      * @param $value directory for storing log files.
@@ -150,7 +130,6 @@ class Logger
             throw new Exception('logPath'."{$value}".' does not point to a valid directory.
 			 Make sure the directory exists and is writable by the Web server process.');
     }
-
     /**
      * @return string 日志文件名称 默认 'application.log'.
      */
@@ -158,7 +137,6 @@ class Logger
     {
         return $this->_logFile;
     }
-
     /**
      * @param string $value 日志文件名称
      */
@@ -166,7 +144,6 @@ class Logger
     {
         $this->_logFile=$value;
     }
-
     /**
      * @return integer maximum 以千字节(KB)的日志文件大小。默认为1024(1 mb)。
      */
@@ -174,7 +151,6 @@ class Logger
     {
         return $this->_maxFileSize;
     }
-
     /**
      * @param integer $value maximum log file size in kilo-bytes (KB).
      */
@@ -183,7 +159,6 @@ class Logger
         if(($this->_maxFileSize=(int)$value)<1)
             $this->_maxFileSize=1;
     }
-
     /**
      * @return integer number 最大文件数
      */
@@ -191,7 +166,6 @@ class Logger
     {
         return $this->_maxLogFiles;
     }
-
     /**
      * @param integer $value 设置最大日志文件数
      */
@@ -200,7 +174,6 @@ class Logger
         if(($this->_maxLogFiles = (int)$value)  < 1)
             $this->_maxLogFiles = 1;
     }
-
     /**
      * warn
      * @param string $value
@@ -211,7 +184,6 @@ class Logger
     {
         return self::write($value, self::LEVEL_WARN, $category);
     }
-
     /**
      * info
      * @param $value
@@ -222,7 +194,6 @@ class Logger
     {
         return self::write($value, self::LEVEL_INFO,  $category);
     }
-
     /**
      * error
      * @param $value
@@ -233,7 +204,6 @@ class Logger
     {
         return self::write($value, self::LEVEL_ERROR,  $category);
     }
-
     /**
      *  debug
      * @param $value
@@ -244,7 +214,6 @@ class Logger
     {
         return self::write($value, self::LEVEL_DEBUG,  $category);
     }
-
     /**
      * 写入日志消息
      * @param $message
@@ -264,7 +233,6 @@ class Logger
         }
         return true;
     }
-
     /**
      * 从内存中删除所有记录的消息。
      */
@@ -274,12 +242,10 @@ class Logger
         $this->_logs = [];
         $this->_logCount=0;
     }
-
     public function onFlush()
     {
         $this->processLogs($this->_logs);
     }
-
     /**
      * 格式化日志信息
      * @param $message
@@ -301,10 +267,8 @@ class Logger
         //17-11-08 10:32:55.189521 <debug>: [Yaf\Application->run] [67720] [127.0.0.1]:ddd
         if($sessionId = session_id()  === '')
             $sessionId = getmypid();
-
         return  sprintf("%s<%s>:[%s][%s][%s] %s(line %s): %s\n", $this->udate('y-m-d H:i:s.u', $time), $level, $category, $sessionId, $ipAddress, $file, $line, $message);
     }
-
     /**
      * 保存日志信息到文件
      * @param array $logs      日志信息
@@ -320,19 +284,16 @@ class Logger
             }
             if(filesize($logFile) > $this->getMaxFileSize()*1024)
                 $this->rotateFiles();
-
             $fp = fopen($logFile,'a');
             flock($fp,LOCK_EX);
             foreach($logs as $log)
                 fwrite($fp,$this->formatLogMessage($log[0], $log[1], $log[2], $log[3], $log[4], $log[5], $log[6]));
-
             flock($fp,LOCK_UN);
             fclose($fp);
         } catch (Exception $e) {
             throw new Exception('log error:'.$e->getMessage());
         }
     }
-
     /**
      * Rotates log files.
      */
@@ -354,7 +315,6 @@ class Logger
         if(is_file($file))
             rename($file,$file.'.1');
     }
-
     /**
      * 生成文件名、行号和函数名
      * @param $message
@@ -387,7 +347,6 @@ class Logger
                 }
             }
         }
-
         if(!empty($category)){
             $category = $category;
         }else if(!empty($traces[1])){
@@ -399,7 +358,6 @@ class Logger
         }
         return  [$message, $level, $category, $time, $file, $line, $traces];
     }
-
     /**
      * 毫秒
      * @param string $strFormat
@@ -414,15 +372,11 @@ class Logger
         }
         $arrTimeStamp = explode('.',$uTimeStamp,2);
         $intMilliseconds = array_pop($arrTimeStamp);
-
         $strMilliseconds = str_pad($intMilliseconds, 4, '0', STR_PAD_LEFT);
         return date(preg_replace('`(?<!\\\\)u`', $strMilliseconds, $strFormat), $arrTimeStamp[0]);
     }
-
     public function __destruct(){
         if($this->_logCount > 0)
             $this->flush();
     }
 }
-
-
