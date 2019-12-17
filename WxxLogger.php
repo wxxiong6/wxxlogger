cd1fba2f951228746ffe71123f72ac46cf0293dc
wxxiong6 <601115211@qq.com>
2019-12-17 11:34:43 +0800

    增加日志模板自定义功能
    增加日志模板自定义功能

--- WxxLogger.php
+++ WxxLogger.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author  wxxiong <wxxiong6@gmail.com>
- * @version v2.0.0
+ * @version v2.0.1
  * @link    https://github.com/wxxiong6/wxxlogger/blob/master/README.md
  */
 namespace wxxiong6\wxxLogger;
@@ -16,7 +16,7 @@ use Exception;
  * * @example
  * <pre>
  *   $config = [
- *      'LogPath' => __DIR__.'/runtime/logs',
+ *      'logPath' => __DIR__.'/runtime/logs',
  *      'maxLogFiles' => 5,
  *      'traceLevel'  => 0,
  *      'maxFileSize' => 10240,
@@ -109,52 +109,86 @@ class WxxLogger
      * @var string 日志文件名称
      */
     private $_logFile = 'application.log';
-
     /**
      * @var bool  是否切割日志文件
      * @since 2.0.0
      */
-    public $enableRotation = true;
-
+    private $_enableRotation = true;
     /**
      * @var bool 开启毫秒.
      * Defaults to false.
      * @since 2.0.0
      */
-    public $microtime = true;
+    private $_microtime = true;
+
+    /**
+     * @var string
+     * %L - Level 日志级别。
+     * %T - DateTime 如2019-12-17 19:17:02
+     * %Q - RequestId 区分单次请求，如没有调用setRequestId($string)方法，则在初始化请求时，采用内置的uniqid()方法生成的惟一值。
+     * %H - HostName 主机名。
+     * %P - ProcessId 进程ID。
+     * %I - Client IP 来源客户端IP; Cli模式下为local。取值优先级为：HTTP_X_REAL_IP > HTTP_X_FORWARDED_FOR > REMOTE_ADDR
+     * %C - Class::Action 类名::方法名，如UserService::getUserInfo。不在类中使用时，记录函数名
+     * %S - 占位符，什么都不做
+     */
+    private $_defaultTemplate = "%T|%L|%P|%I|%Q|%C|%M";
+    /**
+     * @var string 分隔符，必须与$defaultTemplate 一致
+     */
+    private $_separator = '|';
     /**
      * @var WxxLogger
      */
     private static $_instance;
+
     private function __construct()
     {
     }
     private function __clone()
     {
     }
+
     public function __set($name, $value)
     {
-        $property = '_'.$name;
-        $this->$property = $value;
+        $this->$name = $value;
     }
+
     public function __get($name)
     {
         return $this->$name;
     }
+
     public function __call($name, $arguments)
     {
         if (strpos($name, 'set') === 0) {
             $property = str_replace('set', '', $name);
             $property = '_'.lcfirst($property);
             if (isset($arguments[0])) {
-                return $this->$property = $arguments[0];
-            } else {
-                return $this->$property;
+                $this->$property = $arguments[0];
             }
+        } else if (strpos($name, 'get') === 0) {
+            $property = str_replace('get', '', $name);
+            $property = '_'.lcfirst($property);
+            return $this->$property;
         }
     }
+
+    /**
+     * 设置配置文件
+     * @param array $config
+     */
+    public function setConfig(array $config)
+    {
+        foreach ($config as $key => $val) {
+            $func = 'set'.ucfirst($key);
+            call_user_func_array([__CLASS__, $func], [$val]);
+        }
+    }
+
     /**
      * 获取对象
+     * @propery $
      * @return WxxLogger
      */
     public static function getInstance()
@@ -168,24 +202,7 @@ class WxxLogger
         }
         return self::$_instance;
     }
-    /**
-     * 设置配置文件
-     * @param array $config
-     */
-    public function setConfig(array $config)
-    {
-        foreach ($config as $key => $val) {
-            $func = 'set'.ucfirst($key);
-            call_user_func_array([__CLASS__, $func], [$val]);
-        }
-    }
-    /**
-     * @return null|string 存储日志文件目录
-     */
-    public function getLogPath()
-    {
-        return $this->_logPath;
-    }
+
     /**
      *  设置日志目录
      * @param string $logPath 日志目录
@@ -198,6 +215,7 @@ class WxxLogger
         }
         $this->_logPath = $logPath;
     }
+
     /**
      * @return string 日志文件名称 默认 'application.log'.
      */
@@ -205,21 +223,8 @@ class WxxLogger
     {
         return $this->_logPath . DIRECTORY_SEPARATOR . $this->_logFile;
     }
-    /**
-     * 设置日志文件
-     * @param string $value 日志文件名称
-     */
-    public function setLogFile($value)
-    {
-        $this->_logFile =  $value;
-    }
-    /**
-     * @return integer maximum 以千字节(KB)的日志文件大小。默认为1024(1 mb)。
-     */
-    public function getMaxFileSize()
-    {
-        return $this->_maxFileSize;
-    }
+
+
     /**
      * @param integer $value maximum log file size in kilo-bytes (KB).
      */
@@ -229,13 +234,7 @@ class WxxLogger
             $this->_maxFileSize=1;
         }
     }
-    /**
-     * @return integer number 最大文件数
-     */
-    public function getMaxLogFiles()
-    {
-        return $this->_maxLogFiles;
-    }
+
     /**
      * @param integer $value 设置最大日志文件数
      */
@@ -318,30 +317,85 @@ class WxxLogger
         }
         return $obj;
     }
+
     /**
      * 获取日志前缀
      * @param $message
+     * @param $level
+     * @param $category
+     * @param $timestamp
      * @return mixed|string
      */
-    public function getMessagePrefix($message)
+    public function getMessagePrefix($message, $level, $category, $timestamp)
     {
         if ($this->_prefix !== null) {
-            return call_user_func($this->_prefix, $message);
+            return call_user_func($this->_prefix, $message, $level, $category, $timestamp);
         }
-        //获取IP
-        $ip = '0.0.0.0';
-        if (isset($_SERVER["SERVER_ADDR"])) {
-            $ip = $_SERVER["SERVER_ADDR"];
+        $defaultTemplate['%T'] = $this->getTime($timestamp);
+        $defaultTemplate['%L'] = $this->getLevelName($level);
+        $defaultTemplate['%P'] = $this->getProcessId();
+        $defaultTemplate['%Q'] = $this->getRequestId();
+        $defaultTemplate['%I'] = $this->getClientIp();
+        $defaultTemplate['%C'] = $category ?? '-';
+        $defaultTemplate['%S'] = '-';
+        $template = [];
+        $defaultTemplateArr = explode($this->_separator, $this->_defaultTemplate);
+
+        foreach ($defaultTemplateArr as $v) { //按日志模板排序
+            $template[$v] = $defaultTemplate[$v] ?? '';
         }
 
-        if (($sessionID = session_id()) === '') {
-            $sessionID = getmypid();
-        }
-
-        $pID = getmypid();
-
-        return "[$ip][$pID][$sessionID]";
+        return join($this->_separator, $template) . $this->_separator;
     }
+
+    public function getLevelName($level)
+    {
+        if (empty($this->_levelName)) {
+            $this->setLevelName(strtoupper($level));
+        }
+        return $this->_levelName;
+    }
+
+    public function getProcessId()
+    {
+        if (empty($this->_processId)) {
+            $this->setProcessId(getmypid());
+        }
+        return $this->_processId;
+    }
+
+    public function getRequestId()
+    {
+        if (empty($this->_requestId)) {
+            $this->setRequestId(uniqid());
+        }
+        return $this->_requestId;
+    }
+
+    public function getHostName()
+    {
+        if (empty($this->_hostName)) {
+            $this->setHostName(gethostname());
+        }
+        return $this->_hostName;
+    }
+
+    public function getClientIp()
+    {
+        if (empty($this->_clientIp)) {
+            if (!empty($_SERVER['HTTP_X_REAL_IP'])) {
+                return $_SERVER['HTTP_X_REAL_IP'];
+            } else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                return $_SERVER['HTTP_X_FORWARDED_FOR'];
+            } else if (!empty($_SERVER['REMOTE_ADDR'])) {
+                return $_SERVER['REMOTE_ADDR'];
+            }
+        } else
+        {
+            return $this->_clientIp;
+        }
+    }
+
     /**
      * 写入日志。
      */
@@ -353,15 +407,15 @@ class WxxLogger
             $this->_logCount = 0;
         }
     }
-
-
     public function onFlush()
     {
         $this->processLogs($this->_logs);
     }
+
     /**
      * 格式化日志信息
-     * @param array $message
+     * @param array $logs
+     * @return string
      */
     protected function formatMessage(array $logs)
     {
@@ -381,8 +435,9 @@ class WxxLogger
                 }
             }
         }
-        $prefix = $this->getMessagePrefix($message);
-        return  $this->getTime($timestamp) . " {$prefix}[$level][$category] $message "
+        $prefix = $this->getMessagePrefix($message, $level, $category, $timestamp);
+
+        return   " {$prefix} $message "
             . (empty($stack) ? '' : "\n    " . implode("\n    ", $stack));
     }
     /**
@@ -407,19 +462,13 @@ class WxxLogger
                 touch($logFile);
             }
             $text = implode("\n", array_map([$this, 'formatMessage'], $logs)) . "\n";
-
-            if ($this->enableRotation && @filesize($logFile) > ($this->getMaxFileSize() * 1024)) {
+            if ($this->_enableRotation && @filesize($logFile) > ($this->getMaxFileSize() * 1024)) {
                 $this->rotateFiles();
             }
-
             if (($fp = @fopen($logFile, 'a')) === false) {
                 throw new Exception("Unable to append to log file: {$logFile}");
             }
-            #flock($fp, LOCK_EX);
-
             @fwrite($fp, $text);
-
-            #flock($fp, LOCK_UN);
             fclose($fp);
         } catch (Exception $e) {
             throw new Exception('logException:'.$e->getMessage());
@@ -457,34 +506,27 @@ class WxxLogger
     {
         $time = microtime(true);
         $traces = [];
-
-            $ts = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
-            array_pop($ts);
-
-            $count = 0;
-            foreach ($ts as $trace) {
-                if (isset($trace['file'], $trace['line']) &&  strpos($trace['file'], __FILE__) !== 0) {
-                    unset($trace['object'], $trace['args']);
-                    $traces[] = $trace;
-                    if (++$count >= $this->_traceLevel) {
-                        break;
-                    }
-                } elseif (!isset($trace['file'], $trace['line'])) {
-                    $traces[] = $trace;
-                    if (++$count >= $this->_traceLevel) {
-                        break;
-                    }
+        $ts = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+        array_pop($ts);
+        $count = 0;
+        foreach ($ts as $trace) {
+            if (isset($trace['file'], $trace['line']) &&  strpos($trace['file'], __FILE__) !== 0) {
+                unset($trace['object'], $trace['args']);
+                $traces[] = $trace;
+                if (++$count >= $this->_traceLevel) {
+                    break;
+                }
+            } elseif (!isset($trace['file'], $trace['line'])) {
+                $traces[] = $trace;
+                if (++$count >= $this->_traceLevel) {
+                    break;
                 }
             }
-
-
-
-
+        }
         $category = $this->getCategory($category, $traces);
         if ($this->_traceLevel === 0) {
             $traces = [];
         }
-
         return  [$message, $level, $category, $time, $traces];
     }
 
@@ -493,28 +535,28 @@ class WxxLogger
         if ($category !== '-') {
             return $category;
         }
-
         if ($this->_traceLevel === 0) {
             if (isset($traces[0]['file'])) {
                 $pathArr = pathinfo($traces[0]['file']);
                 $category = $pathArr['basename'] . ':' .  $traces[0]['line'];
             }
         } else {
-                if (!empty($traces[1])) {
-                    $category = '';
-                    if (isset($traces[1]['class'])) {
-                        $category .= $traces[1]['class'];
-                    }
-                    if (isset($traces[1]['type'])) {
-                        $category .= $traces[1]['type'];
-                    }
-                    if (isset($traces[1]['function'])) {
-                        $category .= $traces[1]['function'];
-                    }
+            if (!empty($traces[1])) {
+                $category = '';
+                if (isset($traces[1]['class'])) {
+                    $category .= $traces[1]['class'];
+                }
+                if (isset($traces[1]['type'])) {
+                    $category .= $traces[1]['type'];
+                }
+                if (isset($traces[1]['function'])) {
+                    $category .= $traces[1]['function'];
+                }
             }
         }
         return $category;
     }
+
     /**
      * 显示日期时间格式
      * @param null $timestamp 需要格式化的时间戳
@@ -523,8 +565,9 @@ class WxxLogger
      */
     protected function getTime($timestamp)
     {
-         $timestamp = str_replace(',', '.', (string) $timestamp);
-         $parts = explode('.', $timestamp);
-         return date('Y-m-d H:i:s', $parts[0]) . ($this->microtime && isset($parts[1]) ? ('.' . sprintf('%04d', $parts[1])) : '');
+        $timestamp = str_replace(',', '.', (string) $timestamp);
+        $parts = explode('.', $timestamp);
+        return date('Y-m-d H:i:s', $parts[0]) . ($this->_microtime && isset($parts[1]) ? ('.' . sprintf('%04d', $parts[1])) : '');
     }
 }
+
